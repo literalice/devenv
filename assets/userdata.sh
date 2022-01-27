46dbbc779fef77edb0db76f4b8ca3e0e34c175b2
Masatoshi Hayashi <m@literalice.com>
2022-01-27 09:31:20 +0900

    fixes mount logics

--- assets/userdata.sh
+++ assets/userdata.sh
@@ -2,8 +2,7 @@
 
 set -x
 
-sudo systemctl enable amazon-ssm-agent
-sudo systemctl start amazon-ssm-agent
+## Params
 
 volume_id=$1
 user_id=$2
@@ -11,23 +10,25 @@ user_id=$2
 instance_id=$(curl -s 169.254.169.254/latest/meta-data/instance-id)
 export AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//')
 
+
+## Systems Manager
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
+
+## Additional Packages
+yum -y install gcc gcc-c++ make zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl11-devel tk-devel libffi-devel xz-devel
+yum -y install zsh util-linux-user git
+yum -y install nvme-cli
+
 ## AWS CLI
 yum remove -y awscli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
 
-# Disk
+## Disk
 
-yum -y install nvme-cli
-EPHEMERAL_DISK=$(sudo nvme list | grep 'Amazon EC2 NVMe Instance Storage' | awk '{ print $1 }')
-
-## Ephemeral
-mkfs -t xfs $EPHEMERAL_DISK
-mkdir -p /var/lib/docker
-mount $EPHEMERAL_DISK /var/lib/docker
-
-## Prepares a volume for home dir
+## Prepares volumes
 while :; do
     aws ec2 wait volume-available --volume-ids $volume_id && break
 done;
@@ -39,16 +40,24 @@ until [ -e /dev/xvdb ]; do
     sleep 1
 done
 
+## Format
+
+EPHEMERAL_DISK=$(sudo nvme list | grep 'Amazon EC2 NVMe Instance Storage' | awk '{ print $1 }')
+mkfs -t xfs $EPHEMERAL_DISK
+
 if [ "$(file -b -s /dev/xvdb)" == "data" ]; then
     mkfs -t xfs /dev/xvdb
 fi
 
 sleep 1
 
-## Additional Packages
-yum install -y zsh util-linux-user git
-yum install -y gcc gcc-c++ make zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl11-devel tk-devel libffi-devel xz-devel
+## Mount
 
+## Ephemeral for docker storage
+mkdir -p /var/lib/docker
+mount $EPHEMERAL_DISK /var/lib/docker
+
+## EBS for user home
 mkdir -p /home/${user_id}
 mount /dev/xvdb /home/${user_id}
 
@@ -66,6 +75,7 @@ usermod -aG wheel ${user_id}
 echo "${user_id} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/${user_id}
 
 ## Others
+
 ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 
 amazon-linux-extras install -y docker
